2a8fb12c94e0ed13a9f794e8d04d1394c9180bff
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2023-07-23 16:56:33 -0500

    se cambian animaciones de gsap haciendolas verticales

--- js/index.animation.js
+++ js/index.animation.js
@@ -1,10 +1,10 @@
 
-gsap.to(".titulo", { duration: 2.5, ease: "elastic.out(2, 0.4)", y: 25 });
-gsap.from(".card", {fadein: true, x: 1500, duration: 4, opacity: 0 });
-gsap.to(".card", {fadein: true, x: -5, duration: 4, opacity: 1 });
+gsap.from(".titulo", { duration: 2, fadein: true, y: 1250});
+gsap.from(".card", {fadein: true, y: 1500, duration: 3.5, opacity: 0 });
 
-gsap.from(".text", {fadein: true, x: -800, duration: 4, opacity: 0 });
-gsap.to(".text", {fadein: true, x: -5, duration: 4, opacity: 1 });
+gsap.from(".text", {fadein: true, y: 1250, duration: 2.5, opacity: 0 });
+gsap.to(".text", {fadein: true, x: -5, duration: 2.5, opacity: 1 });
+gsap.from(".img-banner", {fadein: true, y: 1250, duration: 2.5, opacity: 1 });
 
 gsap.from(".service", {fadein: true, x: 1500, duration: 4, opacity: 0 });
 gsap.to(".service", {fadein: true, x: -5, duration: 4, opacity: 1 });
@@ -13,9 +13,6 @@ gsap.to(".service", {fadein: true, x: -5, duration: 4, opacity: 1 });
 const portfolioSection = document.getElementById("portafolio");
 
 gsap.registerPlugin(ScrollTrigger);
-
-
-
 
 
 gsap.to(".c",{
