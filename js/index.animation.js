fbd1b752434d30b508cf8254983cb0fdf433859b
Juan Torres <jt158161@gmail.com>
2023-07-25 11:49:39 -0500

    se agrega correcion en las car y se actualiza los enlaces

--- js/index.animation.js
+++ js/index.animation.js
@@ -11,7 +11,7 @@ gsap.to(".text", {fadein: true, x: -5, duration: 2.5, opacity: 1 });
 
 gsap.from(".img-banner", {fadein: true, y: 1250, duration: 2.5, opacity: 1 });
 
-gsap.set(".service", { opacity: 1, x: 1800});
+gsap.set(".service", { opacity: 1, x: 1600});
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -27,7 +27,7 @@ gsap.to(".c",{
 ScrollTrigger.create({
   trigger: ".service",
   start: "top center",
-  onEnter: () => gsap.to(".service", { opacity: 1, x: -20, duration: 6 }),
+  onEnter: () => gsap.to(".service", { opacity: 1, x: -4, duration: 6 }),
 });
 
 // LIBRERIA SWIPER PARA ANIMAR CARRUSEL
